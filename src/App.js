d5a4fffc0205c92be7a88ea2d16599acc407e52f
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-12 15:46:17 +0800

    feat: added static UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './features/Counter';
 
 function App() {
   return (
     <div className="App">
       <h1>Counter</h1>
+      <Counter/>
     </div>
   );
 }
